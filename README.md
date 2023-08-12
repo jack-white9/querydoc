67b4481cc212d86a6c6c6533b54e5b02287a5c1b
Jack White <jfw793@gmail.com>
2023-08-12 10:51:54 +1000

    add custom directory option

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A CLI tool for using text to query documents within a directory.
 ## Usage
 
 ```shell
-ts-node src/index.ts query "<insert query>"
+ts-node src/index.ts query "<insert query>" --dir <insert directory>
 ```
--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,25 @@ import { OpenAIChatCompletionModel } from "./llm/OpenAIChatCompletionModel";
 import { Chroma } from "./vectordb/Chroma";
 import { readFilesInDirectory } from "./util/documents";
 
-const main = async (query: string): Promise<void> => {
+const main = async (query: string, dir: string): Promise<void> => {
   const chroma = new Chroma();
   const collection = await chroma.createCollection();
-  console.log(readFilesInDirectory("documents/"));
-  await chroma.addDocsToCollection(
-    "documents/state_of_the_union.txt",
-    collection
-  );
+  const files = readFilesInDirectory(dir);
+
+  for (let i = 0; i < files.length; i++) {
+    await chroma.addDocsToCollection(files[i], collection);
+  }
+
   const results: QueryResponse = await chroma.queryDocs(collection, query);
   const llm = new OpenAIChatCompletionModel();
   const response = await llm.getGptResponse(query, results.documents[0]);
+
   console.log(response.content);
 };
 
 program
   .command("query <string>")
-  .description("Run a test command.")
-  .action((query) => main(query));
+  .description("Query the documents in a specified directory.")
+  .requiredOption("-d, --dir <directory>", "Specify the document directory")
+  .action((query, options) => main(query, options.dir));
 program.parse();
